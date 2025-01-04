215b06436e7713da8d1f54877ec2e5244d2e1d2d
Petar Radosevic <petar@petar.dev>
2025-01-04 15:40:45 +0100

    colorscheme and treesitter

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
 require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+-- easily run source code
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,9 +1,11 @@
 return {
   {
-     'echasnovski/mini.nvim',
-     config = function()
-	     local statusline = require 'mini.statusline'
-	     statusline.setup { use_icons = true }
-     end
+    'echasnovski/mini.nvim',
+    config = function()
+      local statusline = require 'mini.statusline'
+      statusline.setup { 
+	use_icons = false
+      }
+    end
   }
 }
