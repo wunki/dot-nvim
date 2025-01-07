23c99b34a5938453f35f7ca0cf9005c769966663
Petar Radosevic <petar@petar.dev>
2025-01-07 22:34:15 +0100

    setup sane indenting

--- init.lua
+++ init.lua
@@ -1,24 +1,31 @@
 require("config.lazy")
 
-vim.opt.shiftwidth = 4
+--- sane indentation
+vim.opt.expandtab = true          -- tabs to spaces
+vim.opt.shiftwidth = 2            -- number of spaces
+vim.opt.tabstop = 2               -- number of spaces a tab counts for
+vim.opt.softtabstop = 2           -- number of spaces a tab counts for while editing
+vim.opt.smartindent = true        -- smart autoindenting for newlines
+
 vim.opt.clipboard = "unnamedplus" -- global clipboard
 vim.opt.shortmess:append("I")     -- no startup message
 
---- do we want line numbers?
+--- do we want line numbers? Neh...
 vim.opt.number = false
 vim.opt.relativenumber = false
 
 --- KEYBINDINGS
---- execute the current line
+---
+--- run the current line
 vim.keymap.set("n", "<space>x", function()
   vim.cmd(".lua")
   vim.notify("executed current line", 2)
 end)
 
---- execute selection
+--- run selection
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
---- source the current file
+--- run the current file
 vim.keymap.set("n", "<space><space>x", function()
   vim.cmd("source %")
   vim.notify("sourced current file", 2)
