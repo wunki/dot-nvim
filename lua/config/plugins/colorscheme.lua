682557d229e852d794806a1c2cff6b02a1a795ef
Petar Radosevic <petar@petar.dev>
2025-02-01 15:04:44 -0500

    setup colorschemes

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,21 +1,31 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  opts = {
-    flavour = 'mocha',
-    no_italic = true,
-    color_overrides = {
-      -- make it a little bit darker
-      mocha = {
-        base = '#181825',
-        mantle = '#11111b',
-        crust = '#08080f',
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      flavour = 'latte',
+      no_italic = true,
+      color_overrides = {
+        -- make it a little bit darker
+        mocha = {
+          base = '#181825',
+          mantle = '#11111b',
+          crust = '#08080f',
+        },
       },
     },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      -- vim.cmd.colorscheme 'catppuccin'
+    end,
   },
-  config = function(_, opts)
-    require('catppuccin').setup(opts)
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {},
+    config = function(_, opts)
+      require('nightfox').setup(opts)
+      vim.cmd.colorscheme 'dayfox'
+    end,
+  }
 }
