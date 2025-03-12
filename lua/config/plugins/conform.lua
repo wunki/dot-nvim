ad42ed725882c9444782c48fbd92d5999ef524d7
Petar Radosevic <petar@petar.dev>
2025-03-12 09:36:30 +0100

    conform changes

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -2,11 +2,11 @@ return {
   'stevearc/conform.nvim',
   opts = {
     formatters_by_ft = {
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescriptreact = { 'prettierd', 'prettier' },
+      javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'biome', 'prettierd', 'prettier' },
       html = { 'prettierd', 'prettier', stop_after_first = true },
-      css = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
       svelte = { 'prettierd', 'prettier', stop_after_first = true },
       json = { 'prettierd', 'prettier', stop_after_first = true },
       fish = { 'fish_indent' },
