99e05581224f2d7c0c370197471728e8cddf2581
Petar Radosevic <petar@petar.dev>
2025-04-13 12:59:50 +0200

    chore: update lazy-lock.json and colorscheme config

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,55 +1,7 @@
 return {
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    enabled = false,
-    priority = 1000,
-    opts = {
-      flavour = 'latte',
-      no_italic = true,
-      color_overrides = {
-        -- make it a little bit darker
-        mocha = {
-          base = '#181825',
-          mantle = '#11111b',
-          crust = '#08080f',
-        },
-      },
-    },
-    config = function(_, opts)
-      require('catppuccin').setup(opts)
-      -- vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
-  {
-    'rose-pine/neovim',
-    enabled = false,
-    name = 'rose-pine',
-    config = function()
-      vim.cmd 'colorscheme rose-pine'
-    end,
-  },
-  {
-    'EdenEast/nightfox.nvim',
-    enabled = false,
-    opts = {},
-    config = function(_, opts)
-      require('nightfox').setup(opts)
-      vim.cmd.colorscheme 'dayfox'
-    end,
-  },
-  {
-    'xero/miasma.nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd 'colorscheme miasma'
-    end,
-  },
-  {
     'sainnhe/gruvbox-material',
-    enabled = true,
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
@@ -64,26 +16,19 @@ return {
     end,
   },
   {
-    'folke/tokyonight.nvim',
+    'projekt0n/github-nvim-theme',
     enabled = false,
+    name = 'github-theme',
     lazy = false,
     priority = 1000,
-    opts = {},
-    init = function()
-      vim.cmd 'colorscheme tokyonight-night'
-    end,
-  },
-  {
-    'ficcdaf/ashen.nvim',
-    enabled = false,
-    tag = 'v0.11.0',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      -- your settings here
-    },
-    init = function()
-      vim.cmd 'colorscheme ashen'
+    config = function()
+      require('github-theme').setup {
+        options = {
+          dim_inactive = false,
+        },
+      }
+
+      vim.cmd 'colorscheme github_dark_tritanopia'
     end,
   },
 }
