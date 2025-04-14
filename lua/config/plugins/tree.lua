1e454ecfabfdc2da3b6231d16afe80567052cb68
Petar Radosevic <petar@petar.dev>
2025-04-14 13:07:29 +0200

    chore: don't color the icons

--- lua/config/plugins/tree.lua
+++ lua/config/plugins/tree.lua
@@ -1,7 +1,12 @@
 return {
   'kyazdani42/nvim-tree.lua',
   dependencies = {
-    'kyazdani42/nvim-web-devicons',
+    {
+      'kyazdani42/nvim-web-devicons',
+      config = function()
+        require('nvim-web-devicons').setup({ color_icons = false })
+      end,
+    },
   },
   lazy = false,
   keys = {
