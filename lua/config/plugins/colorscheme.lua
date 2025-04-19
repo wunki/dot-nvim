df10dbd84335235a387b59f794e55aa7a9ef5b07
Petar Radosevic <petar@petar.dev>
2025-04-19 14:04:57 +0200

    colorschemee

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -34,7 +34,7 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'lackluster-dark'
+      vim.cmd.colorscheme 'lackluster-hack'
     end,
   },
   {
@@ -43,7 +43,7 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.cmd 'colorscheme lackluster-dark'
+        vim.cmd 'colorscheme lackluster-hack'
         vim.cmd 'set bg=dark'
       end,
       set_light_mode = function()
