88a2adc105a3c4aa0a752ecadd157bbd2a1de2d0
Petar Radosevic <petar@petar.dev>
2025-09-17 12:12:19 +0200

    cleanup statusline

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -22,20 +22,29 @@ return {
             local git = MiniStatusline.section_git { trunc_width = 40 }
             local diff = MiniStatusline.section_diff { trunc_width = 75 }
             local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
-            local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+            local lsp_icon = (function()
+              local clients = {}
+              if vim.lsp.get_clients then
+                clients = vim.lsp.get_clients { bufnr = 0 }
+              else
+                clients = vim.lsp.get_active_clients { bufnr = 0 }
+              end
+              return (clients and #clients > 0) and '⚙' or ''
+            end)()
+            local devinfo = table.concat(vim.tbl_filter(function(s)
+              return s ~= nil and s ~= ''
+            end, { git, diff, diagnostics, lsp_icon }), ' ')
             local filename = MiniStatusline.section_filename { trunc_width = 140 }
-            local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
-            local location = MiniStatusline.section_location { trunc_width = 75 }
-            local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+            local location = tostring(vim.fn.line '.')
 
             return MiniStatusline.combine_groups {
               { hl = mode_hl, strings = { mode } },
-              { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+              { hl = 'MiniStatuslineDevinfo', strings = { devinfo } },
               '%<', -- Mark general truncate point
               { hl = 'MiniStatuslineFilename', strings = { filename } },
               '%=', -- End left alignment
-              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-              { hl = mode_hl, strings = { search, location } },
+              { hl = mode_hl, strings = { location } },
             }
           end,
         },
