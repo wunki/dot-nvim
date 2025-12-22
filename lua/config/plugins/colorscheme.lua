87ccac309d4b223f38a7b1368bc150546d613ffb
Petar Radosevic <petar@petar.dev>
2025-12-22 16:27:43 +0100

    feat: replace zenbones with flexoki for light theme
    - Add kepano/flexoki-neovim plugin
    - Use flexoki-light for auto-dark-mode light theme
    - Remove zenbones and lush.nvim dependency

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,19 +1,16 @@
 -- Reload the finde colorscheme (useful during development)
 local function reload_finde()
-  -- Clear cached modules
   for name, _ in pairs(package.loaded) do
     if name:match '^finde' then
       package.loaded[name] = nil
     end
   end
-  -- Clear highlights and reload
   vim.cmd 'highlight clear'
   require('finde').setup { gutter = true }
   vim.cmd.colorscheme 'finde'
   vim.notify('Reloaded finde colorscheme', vim.log.levels.INFO)
 end
 
--- Expose globally for easy access
 vim.g.reload_finde = reload_finde
 
 return {
@@ -25,23 +22,14 @@ return {
     config = function()
       require('finde').setup { gutter = false }
       vim.cmd.colorscheme 'finde'
-
-      -- Keybinding to reload my colorscheme
       vim.keymap.set('n', '<leader>ur', reload_finde, { desc = 'Reload finde colorscheme' })
     end,
   },
   {
-    'zenbones-theme/zenbones.nvim',
-    dependencies = 'rktjmp/lush.nvim',
+    'kepano/flexoki-neovim',
+    name = 'flexoki',
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.g.zenbones = {
-        solid_line_nr = true,
-        solid_vert_split = true,
-        lightness = 'bright',
-      }
-    end,
   },
   {
     'f-person/auto-dark-mode.nvim',
@@ -50,12 +38,12 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.cmd 'highlight clear'
-        vim.cmd.colorscheme 'finde-dark'
+        vim.cmd.colorscheme 'finde'
         vim.opt.background = 'dark'
       end,
       set_light_mode = function()
         vim.cmd 'highlight clear'
-        vim.cmd.colorscheme 'zenbones'
+        vim.cmd.colorscheme 'flexoki-light'
         vim.opt.background = 'light'
       end,
     },
