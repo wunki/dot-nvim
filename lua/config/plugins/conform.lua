08f43fd183930ca86c335fe85ea015ba3fcee917
Petar Radosevic <petar@petar.dev>
2025-12-24 07:57:50 +0100

    feat(conform): add dynamic biome/prettier formatter selection

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,17 +1,25 @@
+local function use_biome()
+  return vim.fs.root(0, 'biome.json') ~= nil
+end
+
+local function biome_or_prettier()
+  return use_biome() and { 'biome' } or { 'prettierd' }
+end
+
 return {
   'stevearc/conform.nvim',
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      typescript = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
-      json = { 'prettierd' },
+      javascript = biome_or_prettier,
+      javascriptreact = biome_or_prettier,
+      typescript = biome_or_prettier,
+      typescriptreact = biome_or_prettier,
+      json = biome_or_prettier,
       yaml = { 'prettierd' },
       markdown = { 'prettierd' },
       html = { 'prettierd' },
-      svelte = { 'prettierd' },
+      svelte = biome_or_prettier,
       fish = { 'fish_indent' },
     },
     format_on_save = {
