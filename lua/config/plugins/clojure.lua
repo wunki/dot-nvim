6af2399f5bf8c6bd4162bff79f76b101a22880a1
Petar Radosevic <petar@petar.dev>
2026-03-17 09:05:21 +0100

    feat(clojure): add REPL, structural editing, and rainbow delimiters
    Adds Conjure for nREPL evaluation, nvim-paredit for structural editing, and rainbow-delimiters for paren visibility.

---
+++ lua/config/plugins/clojure.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'Olical/conjure',
+    ft = { 'clojure' },
+  },
+  {
+    'julienvincent/nvim-paredit',
+    ft = { 'clojure' },
+    opts = {},
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    ft = { 'clojure' },
+  },
+}
